b09214e031bc5a299bb63586067f22b1f64cf2b9
tamyresrsm <tamyres.rsmagalhaes@senacsp.edu.br>
2025-02-26 19:58:47 -0300

    feat: teste

---
+++ script.js
@@ -0,0 +1,56 @@
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperação</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+
+    <div class="titulo">
+        <form action="">
+            <h1>Login</h1>
+            <br> <br> <br>
+            <p class="h6">Username</p>
+            <div class="input-container">
+                <input type="text" placeholder="Type your username" required>
+                <i class='bx bx-user'></i>
+            </div>
+            <br>
+            <p class="h6">Password</p>
+            <div class="input-container">
+                <input type="password" placeholder="Type your password" required>
+                <i class='bx bx-lock'></i>
+            </div>
+
+
+            <div class="forget">
+                <a href="#">Forget password?</a>
+            </div>
+            <br>
+            <button type="submit" class="btn">LOGIN</button>
+
+            <p>Or Sign Up Using</p>
+            <div class="social-icons">
+                <a href="#" class="facebook"><i class='bx bxl-facebook'></i></a>
+                <a href="#" class="twitter"><i class='bx bxl-twitter'></i></a>
+                <a href="#" class="google"><i class='bx bxl-google'></i></a>
+            </div>
+            <br><br><br><br>
+            <div class="forget-link">
+                <p href="#">Have not account yet?</p>
+            </div>
+            <br>
+            <div class="forget-link">
+                <a href="#">SIGN UP</a>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
